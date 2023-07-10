32a9914cde2a0cfebeda43249bc3ce83fc5a9853
graceannik <109421734+graceannik@users.noreply.github.com>
2023-07-09 22:26:23 -0400

    Footer.js
    Bas de page de mon site web

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.css'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <p className='contain'>  &copy; Copyright Grace Annick Djussi </p>  
+        <p className='contain'> <a href='https://github.com/graceannik/portfolio' className='link'> Git hub </a> </p>
+
+        <p className='contain'> <a href='https://www.linkedin.com/in/djussi-grace-43089923b/' className='link'> Mon profil linkedin </a> </p>
+    </div>
+  )
+}
