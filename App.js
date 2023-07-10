477b4bde039f557d73f2d42a1a07fd707a5dbed7
graceannik <109421734+graceannik@users.noreply.github.com>
2023-07-09 22:43:46 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Layout from './components/Layout';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+         
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
